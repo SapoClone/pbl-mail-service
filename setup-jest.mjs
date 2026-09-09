8844cddbd84427f8def0449830b1dca7df735381
TruongDayNe <lostpeanut123@gmail.com>
2026-09-09 22:36:30 +0700

    chore: scaffold pbl-mail-service

---
+++ setup-jest.mjs
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+process.env.NODE_ENV = 'test';
+process.env.APP_NAME = 'NestJS Mail Service';
+process.env.APP_URL = 'http://localhost:3001';
+process.env.APP_PORT = '3001';
+process.env.APP_DEBUG = 'false';
+process.env.APP_LOG_LEVEL = 'debug';
+process.env.APP_LOG_SERVICE = 'console';
+process.env.RESEND_API_KEY = 'test-api-key';
+process.env.MAIL_DEFAULT_EMAIL = 'noreply@example.com';
+process.env.MAIL_DEFAULT_NAME = 'No Reply';
